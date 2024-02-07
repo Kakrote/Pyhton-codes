cb461817010e9c18d4ea1818536b17a8f533c0fd
Kakrote <anshul.pundir111@gmail.com>
2024-02-07 23:04:21 +0530

    sxsd

---
+++ CL_01.py
@@ -0,0 +1,9 @@
+# the basics of variable is in this code.
+var1="30"
+var2="20"
+var3=var1+var2
+print(var3)
+#now let say we are expapcting the result 50 not 3020 then. we need to type caste it var1 and var 2 into integer
+a=int(var1)
+b=int(var2)
+print(a+b) # now we will get the 50 as answer.
