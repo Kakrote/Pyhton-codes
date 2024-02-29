007f0150070f34e6bcb2a9726a9bf9c19ad4b655
Anshul <anshul.pundir111@gmail.com>
2024-02-29 18:38:13 +0530

    gjgh

---
+++ else_with_forloop.py
@@ -0,0 +1,10 @@
+list=["anshul ","mohit","nikhil"]
+# for i in list:
+#     print(i)
+# else: # this else part will print .
+#     print("this loop ends ")
+for j in list:
+    if j=="mandeep":
+        break
+else:
+    print("this loop is ended here.")
---
+++ practice/heloo.py
@@ -0,0 +1,8 @@
+import math as m
+def foo(x):
+    if x>0:
+        print(m.sqrt(x))
+        # return m.sqrt(x)
+    else:
+        print("sorry!")
+print(foo(144))
