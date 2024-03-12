aaf56c4f754f3d8089146c1a54e799d6f69a4abf
Kakrote <anshul.pundir111@gmail.com1>
2024-03-12 21:57:36 +0530

    hhh

--- Downlord_yt.py
+++ Downlord_yt.py
@@ -1,8 +1,12 @@
 from pytube import YouTube as yt
-link=yt("") # paste link here;
-video=link.streams.get_highest_resolution()
-import os
-os.chdir("C://Users//anshu//Downloads")
-# print(os.listdir())
+try:
+    link=yt("") # paste link here;
+    video=link.streams.get_highest_resolution()
+    import os
+    os.chdir("C://Users//anshu//Downloads")
+    # print(os.listdir())
 
-video.download()
+    video.download()
+except Exception as e:
+    print(e)
+    
